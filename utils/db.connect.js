a2849512f66bb420180078a88cd323a01084aa00
harshit-0606 <sharma.harshit0606@gmail.com>
2025-05-20 18:02:06 +0530

    add env file

--- utils/db.connect.js
+++ utils/db.connect.js
@@ -3,12 +3,12 @@ const mongoDb = require('mongodb');
 
 const mongoClient = mongoDb.MongoClient;
 
-const MONGO_URI = "mongodb+srv://harshit:mongodb@cluster0.wo0mj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 
 let _db;
 
 const mongoConnect = (callback)=>{
-    mongoClient.connect(MONGO_URI)
+    mongoClient.connect(process.env.MONGO_URI)
     .then((client)=>{
         console.log("Connected to mongoDB");
         _db = client.db('airbnb');
